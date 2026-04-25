705caf4119739003c5040a6ae12f746f54432901
MerttBodur <mert.bodur@bahcesehir.edu.tr>
2026-04-25 21:05:36 +0300

    chore: setup contracts dependencies and network config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "hardhat/config";
+import { configVariable, defineConfig } from "hardhat/config";
 
 export default defineConfig({
-  solidity: {
-    version: "0.8.28",
+  solidity: { version: "0.8.28" },
+  networks: {
+    baseSepolia: {
+      type: "http",
+      chainType: "op",
+      url: configVariable("BASE_SEPOLIA_RPC_URL"),
+      accounts: [configVariable("PRIVATE_KEY")],
+    },
   },
 });
